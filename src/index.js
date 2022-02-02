4e223f54207f562db417514974938d780821df96
hossainalamin <hossainalamin980@gmail.com>
2022-02-02 23:45:11 +0600

    jsx attributes

--- src/index.js
+++ src/index.js
@@ -5,21 +5,32 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 const name = "Md Alamin hossain"
 var currentDate = new Date()
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/300/300";
 ReactDOM.render(
-  [
-  <h1>Top five favourite movies I watched</h1>,
-  <p>Here is the list:</p>,
-  <ol>
-    <li>Life is beautifu</li>
-    <li>Itihash</li>
-    <li>Zindegi na Milegegi Doobara</li>
-    <li>Ammajan</li>
-    <li>Hazar bochor Dhore</li>
-  </ol>,
-  <h1>{`My name is ${name}`}</h1>,
-  <p>{`Current date ${currentDate.toLocaleDateString()}`}</p>,
-  <p>{`Current time ${currentDate.toLocaleTimeString()}`}</p>
-  ],
+  //[
+  // <h1>Top five favourite movies I watched</h1>,
+  // <p>Here is the list:</p>,
+  // <ol>
+  //   <li>Life is beautifu</li>
+  //   <li>Itihash</li>
+  //   <li>Zindegi na Milegegi Doobara</li>
+  //   <li>Ammajan</li>
+  //   <li>Hazar bochor Dhore</li>
+  // </ol>,
+  // <h1>{`My name is ${name}`}</h1>,
+  // <p>{`Current date ${currentDate.toLocaleDateString()}`}</p>,
+  // <p>{`Current time ${currentDate.toLocaleTimeString()}`}</p>
+  // ],
+  <>
+  <h1 contentEditable="true" className="name">My name is {name}</h1>
+  <div className='img-div'>
+    <img src={img1} alt="Lorem image"/>
+    <img src={img2} alt="Lorem image"/>
+    <img src={img3} alt="Lorem image"/>
+  </div>
+  </>,
   document.querySelector("#root")
 )
 // ReactDom.render(
