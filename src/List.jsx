369da40007c476cf88480eaf281c9f5cad0586cc
hossainalamin <hossainalamin980@gmail.com>
2022-02-04 00:18:44 +0600

    components

---
+++ src/List.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function List(){
+    return (
+    <ol>
+        <li>Rokib</li>
+        <li>Kanon</li>
+        <li>Yeasin</li>
+        <li>Sajib</li>
+    </ol>);
+}
+export default List;
--- src/index.js
+++ src/index.js
@@ -3,26 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-const name = "Md Alamin hossain"
-var currentDate = new Date()
-currentDate = currentDate.getHours();
-var greeting = " ";
-var cssStyle = {};
-if(currentDate >= 1 && currentDate < 12){
-    greeting = "Good morming";
-    cssStyle.color = "green";
-}else if(currentDate >= 12 && currentDate < 19){
-  greeting = "Good Afternoon";
-  cssStyle.color = "Orange";
-}else{
-  greeting = "Good night"
-  cssStyle.color = "Black"
-}
 ReactDOM.render(
   <>
-  <div>
-    <h1 >Hello sir, <span style={cssStyle}>{greeting}</span></h1>
-  </div>
+  <App/>
   </>,
   document.querySelector("#root")
 )
