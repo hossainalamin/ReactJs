0866922fb407542bd13c88faea58fc00827ea971
hossainalamin <hossainalamin980@gmail.com>
2022-02-10 23:55:05 +0600

    form event

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,20 +1,39 @@
 import React, { useState } from "react";
 const Form = ()=>{
-    const[myName,setName] = useState()
-    const[name,setFormname] = useState()
+    const[fullname,setFormname] = useState({
+        fname : ' ',
+        lname : ' '
+    });
     const formData = (event)=>{
-        setName(event.target.value)
-    }
-    const submitData = ()=>{
-        setFormname(myName)
-    }
+       const value =  event.target.value
+       const name =  event.target.name
+       setFormname((preVlaue)=>{
+        if(name === 'fname' ){
+            return{
+            fname : value,
+            lname : preVlaue.lname
+            }
+        }else if(name === 'lname' ){
+            return{
+            fname : preVlaue.fname,
+            lname : value
+            }
+        }
+    });
+}
+    // const submitData = ()=>{
+        
+    // }
     return (
         <>
-        <div>
-            <h2 style={{justifyContent : 'center',display : 'flex'}}>Hello {name}</h2>
-            <input type="text" placeholder = "Enter your name" onChange={formData}/><br/><br/>
-            <button className="btn btn-success" onClick={submitData}>Click Me</button>
+        <form action="">
+        <div style={{height : '330px'}}>
+            <h2 >Hello {fullname.fname} {fullname.lname}</h2>
+            <input type="text" placeholder = "Enter first name" style={{width:'300px'}} name = "fname" onChange={formData}/><br/><br/>
+            <input type="text" placeholder = "Enter last name" style={{width:'300px'}} name = "lname" onChange={formData}/><br/><br/>
+            <button className="btn btn-success">Click Me</button>
          </div>   
+         </form>
         </>
     )
 }
